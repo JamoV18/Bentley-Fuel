3779d16c2f4b2524fb42a7342aab87b647a19268
JamoV18 <jvelez@falcon.bentley.edu>
2026-08-19 11:36:55 -0400

    Add Today nutrition tracking page

---
+++ src/app/today/page.tsx
@@ -0,0 +1,17 @@
+import { getDiningProvider } from "@/services";
+import TodayClient from "./TodayClient";
+
+export default async function TodayPage() {
+  const provider = getDiningProvider();
+  const [locations, menuItems] = await Promise.all([
+    provider.getLocations(),
+    provider.getMenuItems(),
+  ]);
+  return (
+    <TodayClient
+      locationNames={Object.fromEntries(locations.map((location) => [location.id, location.shortName ?? location.name]))}
+      itemNames={Object.fromEntries(menuItems.map((item) => [item.id, item.name]))}
+      isDemo={provider.dataStatus === "mock"}
+    />
+  );
+}

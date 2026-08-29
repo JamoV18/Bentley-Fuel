a7ec4215f3991d0e302a4a5423d8614f0c40ff48
JamoV18 <jvelez@falcon.bentley.edu>
2026-08-28 20:32:39 -0400

    Add isolated onboarding preview repositories

---
+++ src/services/onboardingPreviewRepositories.ts
@@ -0,0 +1,14 @@
+import type { ProfileRepository } from "./profileRepository";
+import type { ProgressRepository } from "./progressRepository";
+
+export const onboardingPreviewProfileRepository: ProfileRepository = {
+  get: () => null,
+  save: () => undefined,
+  clear: () => undefined,
+};
+
+export const onboardingPreviewProgressRepository: ProgressRepository = {
+  getRecent: () => [],
+  upsert: () => undefined,
+  clear: () => undefined,
+};

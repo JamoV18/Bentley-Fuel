c0d04164e6b193a7b35db8f948f38387719ef271
JamoV18 <jvelez@falcon.bentley.edu>
2026-09-14 18:29:54 -0400

    Wire 921 daily sync admin page

---
+++ src/app/admin/921-sync/page.tsx
@@ -0,0 +1,10 @@
+import SyncClient from "./SyncClient";
+import "./sync.css";
+
+export const metadata = {
+  title: "921 Daily Sync · Falcon Fuel",
+};
+
+export default function Daily921SyncPage() {
+  return <SyncClient />;
+}
